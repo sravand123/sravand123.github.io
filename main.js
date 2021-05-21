72df8a22c4dfaf50ec621706d4fe4a2a49b4cb1a
Sravan Kumar Dasari <sravannn52@gmail.com>
2021-05-21 11:42:49 +0530

    added videos

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 
-const projects  = document.querySelector('#projects')
-const skills    = document.querySelector('#skills');
-const experiences = document.querySelector('#experiences');
 
 
-projects.addEventListener('click',()=>{
-    document.getElementById("project-content").scrollIntoView();
+var list = ["projects","skills","experiences"]
+
+var view = function(x) {
+    const element = document.getElementById(x);
+    element.addEventListener('click',()=>{
+        document.getElementById(x+"-content").scrollIntoView();
+    })
+}
+
+var g = list.map((el,id)=>{
+    view(el)
 })
-skills.addEventListener('click',()=>{
-    document.getElementById("skill-content").scrollIntoView();
+
+var projects = ["KoraaKaagaz","SecureSign"];
+
+
+var attachListeners = function(x) {
+    const element = document.getElementById(x);
+    element.addEventListener('click',()=>{
+        document.getElementById(x+'-video').classList.remove('hidden');
+        document.getElementsByTagName('body')[0].scrollIntoView();
+    })
+    document.getElementById(x+'-close').addEventListener('click',()=>{
+        stopVideo(document.getElementById(x+'-video'));
+        document.getElementById(x+'-video').classList.add('hidden');
+        document.getElementById("projects-content").scrollIntoView();
+    
+    })
+}
+
+var g = projects.map((el,id)=>{
+    attachListeners(el);
 })
-experiences.addEventListener('click',()=>{
-    document.getElementById("experience-content").scrollIntoView();
-})
+
+var stopVideo = function ( element ) {
+	var iframe = element.querySelector( 'iframe');
+	var video = element.querySelector( 'video' );
+	if ( iframe ) {
+		var iframeSrc = iframe.src;
+		iframe.src = iframeSrc;
+	}
+	if ( video ) {
+		video.pause();
+	}
+};
